b0ffd0aea63e3da2147248b77e951b3fb2d050f8
tily <tily05@gmail.com>
2013-07-16 21:30:51 +0900

    update source.

--- lib/chef-serverspec-handler.rb
+++ lib/chef-serverspec-handler.rb
@@ -2,7 +2,6 @@ require 'erb'
 require 'fileutils'
 require 'chef'
 
-# TODO: chef log output
 class ChefServerspecHandler < Chef::Handler
 
   SUPPORTED_RESOURCES = {
@@ -34,30 +33,32 @@ class ChefServerspecHandler < Chef::Handler
     if !FileTest.directory?(options[:dir])
       raise ArgumentError, "directory #{options[:dir]} does not exist"
     end
-    @resources_by_recipe = Hash.new{|h,k| h[k] = Hash.new{|h,k| h[k] = []}}
   end
 
   def report
+    @target_resources = extract_target_resources
+    @target_resources.each do |cookbook, recipes|
+      cookbook_dir = File.join(@dir, cookbook.to_s)
+      FileUtils.mkdir_p cookbook_dir
+      recipes.each do |recipe, resources|
+        spec_path = File.join(cookbook_dir, "#{recipe}_spec.rb")
+        # override existing files?
+        # TODO: chef log output
+        Chef::Log.info "generating spec: #{spec_path}"
+        template = File.read(File.dirname(__FILE__) + '/chef-serverspec-handler.erb')
+        spec = ERB.new(template, nil, '-').result(binding)
+        File.open(spec_path, 'w') {|f| f.write(spec) }
+      end
+    end
+  end
+
+  def extract_target_resources
+    @resources_by_recipe = Hash.new{|h,k| h[k] = Hash.new{|h,k| h[k] = []}}
     all_resources.each do |resource|
       if SUPPORTED_RESOURCES.keys.include?(resource.class)
         actions = [resource.action].flatten.map {|a| a.to_sym }
         if (SUPPORTED_RESOURCES[resource.class] & actions).size > 0
           @resources_by_recipe[resource.cookbook_name][resource.recipe_name] << resource
-        end
-      end
-    end
-
-    @resources_by_recipe.each do |cookbook, recipes|
-      cookbook_dir = File.join(@dir, cookbook.to_s)
-      FileUtils.mkdir_p cookbook_dir
-
-      recipes.each do |recipe, resources|
-        spec_path = File.join(cookbook_dir, "#{recipe}_spec.rb")
-        Chef::Log.info "generating spec: #{spec_path}"
-        template = File.read(File.dirname(__FILE__) + '/chef-serverspec-handler.erb')
-        spec = ERB.new(template, nil, '-').result(binding)
-        File.open(spec_path, 'w') do |f|
-          f.write(spec)
         end
       end
     end
