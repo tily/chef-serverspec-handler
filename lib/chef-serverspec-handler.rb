a5a35af22f909da497a2cd2ce04501b6a906dbfa
tily <tily05@gmail.com>
2013-07-21 12:58:17 +0900

    update files.

--- lib/chef-serverspec-handler.rb
+++ lib/chef-serverspec-handler.rb
@@ -1,9 +1,12 @@
-require 'erb'
+require 'erubis'
 require 'fileutils'
 require 'chef'
 
 class ChefServerspecHandler < Chef::Handler
 
+  HERE =  File.absolute_path File.dirname(__FILE__) 
+
+  # TODO: route, ifconfig, scm, git, subversion, chef_gem, gem_package, easy_install_package
   SUPPORTED_RESOURCES = {
     Chef::Resource::File            => [:create, :create_if_missing, :touch],
     Chef::Resource::CookbookFile    => [:create, :create_if_missing],
@@ -21,47 +24,64 @@ class ChefServerspecHandler < Chef::Handler
     Chef::Resource::User            => [:create],
     Chef::Resource::Group           => [:create],
     Chef::Resource::Service         => [:start, :enable],
-    Chef::Resource::Cron            => [:create],
-    Chef::Resource::Route           => [:add]
+    Chef::Resource::Cron            => [:create]
   }
 
   def initialize(options={})
     @options = options
-    if options[:dir].nil?
-      raise ArgumentError, 'option :dir must be specified'
+
+    if @options[:output_dir].nil?
+      raise ArgumentError, 'option :output_dir is required'
     end
-    if !FileTest.directory?(options[:dir])
-      raise ArgumentError, "directory #{options[:dir]} does not exist"
+
+    if !FileTest.directory?(@options[:output_dir])
+      raise ArgumentError, ":ouput_dir is not directory"
     end
+
+    @options[:force] ||= false
   end
 
   def report
-    @target_resources = extract_target_resources
-    @target_resources.each do |cookbook, recipes|
-      cookbook_dir = File.join(@dir, cookbook.to_s)
+    resources = extract_target_resources
+
+    resources.each do |cookbook, recipes|
+      cookbook_dir = File.join(@options[:output_dir], cookbook.to_s)
       FileUtils.mkdir_p cookbook_dir
+
       recipes.each do |recipe, resources|
         spec_path = File.join(cookbook_dir, "#{recipe}_spec.rb")
-        # override existing files?
-        # TODO: chef log output
-        Chef::Log.info "generating spec: #{spec_path}"
-        template = File.read(File.dirname(__FILE__) + '/chef-serverspec-handler.erb')
-        spec = ERB.new(template, nil, '-').result(binding)
-        File.open(spec_path, 'w') {|f| f.write(spec) }
+
+        if File.exists?(spec_path) && !@options[:force]
+          Chef::Log.warn "ChefServerspecHandler: #{cookbook}/#{recipe}.rb already exists. set :force option to true if you want to override it"
+          next
+        end
+
+        Chef::Log.info "generating #{cookbook}/#{recipe}_spec.rb"
+
+        File.open(spec_path, 'w') do |file|
+          template = File.read File.join(HERE, 'chef-serverspec-handler.erb')
+          spec = ERB.new(template, nil, '-').result(binding)
+          file.write(spec)
+        end
       end
     end
   end
 
   def extract_target_resources
-    @resources_by_recipe = Hash.new{|h,k| h[k] = Hash.new{|h,k| h[k] = []}}
+    resources = Hash.new{|h,k| h[k] = Hash.new{|h,k| h[k] = []}}
+
     all_resources.each do |resource|
       if SUPPORTED_RESOURCES.keys.include?(resource.class)
-        actions = [resource.action].flatten.map {|a| a.to_sym }
-        if (SUPPORTED_RESOURCES[resource.class] & actions).size > 0
-          @resources_by_recipe[resource.cookbook_name][resource.recipe_name] << resource
+        if (SUPPORTED_RESOURCES[resource.class] & regular_actions(resource)).size > 0
+          resources[resource.cookbook_name][resource.recipe_name] << resource
         end
       end
     end
+
+    resources
+  end
+
+  def regular_actions(resource)
+    [resource.action].flatten.map {|action| action.to_sym }
   end
 end
-
